76bbbbd1efb741f4853c8ad165fc4ce99417e006
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-07 00:50:39 +0600

    feat: connect with mysql database

--- pharmacy_management/settings.py
+++ pharmacy_management/settings.py
@@ -73,10 +73,24 @@ WSGI_APPLICATION = 'pharmacy_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_pharmacy',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', 
+        'PORT': '3306',      
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
