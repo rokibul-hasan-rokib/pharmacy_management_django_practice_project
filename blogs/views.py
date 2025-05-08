fed85cff8c0bd92dca2cb784a799407c8fa0a31d
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-09 03:14:00 +0600

    feat: implement CRUD operations for Blog model in views

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Blog
+from .forms import BlogForm
+
+def blog_list(request):
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blog/blog_list.html', context)
+
+def blog_detail(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    context = {'blog': blog}
+    return render(request, 'blog/blog_detail.html', context)
+
+def blog_create(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm()
+    context = {'form': form}
+    return render(request, 'blog/blog_form.html', context)
+
+def blog_update(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm(instance=blog)
+    context = {'form': form}
+    return render(request, 'blog/blog_form.html', context)
+
+def blog_delete(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('blog_list')
+    context = {'blog': blog}
+    return render(request, 'blog/blog_delete.html', context)
