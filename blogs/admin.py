9d513398ed68db763b9b54589649b19269a22f76
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-09 00:20:36 +0600

    feat: register Blog model in admin with custom admin options

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title', 'description')
+    list_filter = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
