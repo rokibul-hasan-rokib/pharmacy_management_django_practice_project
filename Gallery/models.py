49e2a2194da7b6a6aa02ec7b555b1d7221a66374
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-09 03:21:35 +0600

    feat: define Gallery and GalleryImage models with fields and string representations

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class GalleryImage(models.Model):
+    gallery = models.ForeignKey(Gallery, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='gallery_images/')
+
+    def __str__(self):
+        return f"Image for {self.gallery.name}"
