82390b53a66668f60c1b2a630d6a16eb0d0b8008
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-09 00:19:55 +0600

    feat: add media URL and root settings to serve media files

--- pharmacy_management/settings.py
+++ pharmacy_management/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
