5fef262730589b81f387146e65566556d86e9a20
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-12 23:57:05 +0600

    feat: implement contact view for handling form submissions and success messages

--- Contact/views.py
+++ Contact/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm
+from django.contrib import messages
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {'form': form})\
+    
+def thank_you(request):
+    return render(request, 'contact/thank_you.html')
