0c1c8287d8704b49c00cec80acb04f6fccd9dfdb
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-12 23:00:23 +0600

    feat: register Contact model in admin with display and search options

--- Contact/admin.py
+++ Contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Contact
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
+    search_fields = ('name', 'email')
+    list_filter = ('name',)
