6ae7bcda5932d19cc75ab9d8a50b8902120fc6cb
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-13 23:45:29 +0600

    feat: register User model in admin with custom display and search options

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address')
+    search_fields = ('name', 'email')
+    list_filter = ('phone',)
