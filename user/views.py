89ebae390468b73147b7ea198b6c35173c690dbb
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-14 00:23:07 +0600

    feat: implement CRUD views for User model with list, detail, create, update, and delete functionality

--- user/views.py
+++ user/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import User
+
+class UserList(ListView):
+    model = User
+    template_name = 'user/user_list.html'
+    context_object_name = 'users'
+
+class UserDetail(DetailView):
+    model = User
+    template_name = 'user/user_detail.html'
+    context_object_name = 'user'
+
+class UserCreate(CreateView):
+    model = User
+    template_name = 'user/user_form.html'
+    fields = ['name', 'email', 'password', 'phone', 'address']
+    success_url = reverse_lazy('user-list')
+
+class UserUpdate(UpdateView):
+    model = User
+    template_name = 'user/user_form.html'
+    fields = ['name', 'email', 'password', 'phone', 'address']
+    success_url = reverse_lazy('user-list')
+
+class UserDelete(DeleteView):
+    model = User
+    template_name = 'user/user_confirm_delete.html'
+    context_object_name = 'user'
+    success_url = reverse_lazy('user-list')
