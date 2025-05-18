5fc13955f59bcd9b06dfc1f76b0e96ff0396c7e9
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-19 01:00:23 +0600

    feat: update Cabin model to use integer choices for cabin types

--- cabin/models.py
+++ cabin/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Cabin(models.Model):
     CABIN_TYPE_CHOICES = [
-        ('ECONOMY', 'Economy'),
-        ('DELUXE', 'Deluxe'),
-        ('SUITE', 'Suite'),
+        (1, 'Economy'),
+        (2, 'Deluxe'),
+        (3, 'Suite'),
     ]
 
     cabin_number = models.CharField(max_length=100)
-    cabin_type = models.CharField(max_length=100, choices=CABIN_TYPE_CHOICES)
+    cabin_type = models.PositiveSmallIntegerField(choices=CABIN_TYPE_CHOICES)
     cabin_price = models.CharField(max_length=100)
 
     class Meta:
@@ -16,3 +16,4 @@ class Cabin(models.Model):
 
     def __str__(self):
         return self.cabin_number
+
