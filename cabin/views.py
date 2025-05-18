e975abf15516380d81b9085fdb31f9f8e8b08bc8
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-19 01:03:52 +0600

    feat: implement CRUD views for Cabin model in the admin interface

--- cabin/views.py
+++ cabin/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Cabin   
+
+class CabinList(ListView):
+    model = Cabin
+    template_name = 'cabin/cabin_list.html'
+    context_object_name = 'cabins'
+
+class CabinDetail(DetailView):
+    model = Cabin
+    template_name = 'cabin/cabin_detail.html'
+    context_object_name = 'cabin'
+
+class CabinCreate(CreateView):
+    model = Cabin
+    template_name = 'cabin/cabin_form.html'
+    fields = ['name', 'location', 'capacity']
+    success_url = reverse_lazy('cabin-list')
+
+class CabinUpdate(UpdateView):
+    model = Cabin
+    template_name = 'cabin/cabin_form.html'
+    fields = ['name', 'location', 'capacity']
+    success_url = reverse_lazy('cabin-list')
+
+class CabinDelete(DeleteView):
+    model = Cabin
+    template_name = 'cabin/cabin_confirm_delete.html'
+    context_object_name = 'cabin'
+    success_url = reverse_lazy('cabin-list')
+
+    
