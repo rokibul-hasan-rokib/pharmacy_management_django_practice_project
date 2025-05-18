ff00ff4a44280a0a4c3f2d5e52dda8ecc23237b0
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-19 00:50:08 +0600

    feat: disable delete permission for User model in admin

--- user/admin.py
+++ user/admin.py
@@ -7,3 +7,6 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'address')
     search_fields = ('name', 'email')
     list_filter = ('phone',)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
