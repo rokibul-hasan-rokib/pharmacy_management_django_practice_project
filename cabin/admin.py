34d3b636cf6fe064bcfe513108ffa9ed98fd065e
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-19 01:01:38 +0600

    feat: implement CabinAdmin for managing Cabin model in admin interface

--- cabin/admin.py
+++ cabin/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Cabin
+
+# Register your models here.
+class CabinAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'capacity')
+    search_fields = ('name', 'location')
+    list_filter = ('location',)
+
+
+admin.site.register(Cabin)
