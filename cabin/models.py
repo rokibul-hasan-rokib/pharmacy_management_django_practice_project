20c3a8a5fa911dded3319dbb5a6b505b982d07e4
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-19 00:55:33 +0600

    feat: define Cabin model with attributes and choices for cabin types

--- cabin/models.py
+++ cabin/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Cabin(models.Model):
+    CABIN_TYPE_CHOICES = [
+        ('ECONOMY', 'Economy'),
+        ('DELUXE', 'Deluxe'),
+        ('SUITE', 'Suite'),
+    ]
+
+    cabin_number = models.CharField(max_length=100)
+    cabin_type = models.CharField(max_length=100, choices=CABIN_TYPE_CHOICES)
+    cabin_price = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'cabins'
+
+    def __str__(self):
+        return self.cabin_number
