32eedfdf85f2611a6e32fd246d2460a62b9ee288
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-20 22:18:48 +0600

    feat: implement EmployeeAdmin class for managing Employee model in admin interface

--- employee/admin.py
+++ employee/admin.py
@@ -3,3 +3,14 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Employee
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'designation', 'status')
+    search_fields = ('name', 'designation')
+    list_filter = ('status',)
+
+    class Meta:
+        model = Employee
+
+
+admin.site.register(Employee, EmployeeAdmin)
