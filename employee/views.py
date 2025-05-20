7d9f8a02635d67a5c351374714f2aa3b6c6533bf
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-20 22:19:48 +0600

    feat: implement EmployeeList view for displaying employee records

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Employee
+
+class EmployeeList(ListView):
+    model = Employee
+    template_name = 'employee/employee_list.html'
+    context_object_name = 'employees'
