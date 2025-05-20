b15ba55f6f658b32255185297554d40c240f574c
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-20 22:19:16 +0600

    feat: add __str__ method to Employee model for better string representation

--- employee/models.py
+++ employee/models.py
@@ -6,3 +6,6 @@ class Employee(models.Model):
     name = models.CharField(max_length=100)
     designation = models.CharField(max_length=100)
     status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
