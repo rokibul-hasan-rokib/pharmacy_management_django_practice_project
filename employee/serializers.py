56b0d4dd511b685a4e88a1c70e98eb427b266461
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-21 22:50:41 +0600

    fix: add missing newline at the end of EmployeeSerializer

--- employee/serializers.py
+++ employee/serializers.py
@@ -7,5 +7,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'designation', 'status']
         read_only_fields = ['id']
 
+
     def create(self, validated_data):
         return Employee.objects.create(**validated_data)
